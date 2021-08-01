d02113a1619426a1a7bd82da12a8a20f657d3197
GidBerridge <78305510+GidBerridge@users.noreply.github.com>
2021-08-01 16:02:24 +0100

    first commit

---
+++ Refuge/index.js
@@ -0,0 +1,18 @@
+
+import Typed from 'typed.js';
+
+// const typed = new Typed('#typed', {
+//   // Waits 1000ms after typing "First"
+//   strings: ['New EP Illusions of Granduer', 'See Music section to stream']
+
+// });
+
+const options = {
+  strings: ['New EP Illusions of Granduer', 'See Music section to stream'],
+  typeSpeed: 40
+  console.log('test')
+};
+
+const typed = new Typed('#typed', options);
+
+export
