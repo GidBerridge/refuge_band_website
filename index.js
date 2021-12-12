490d2a915f827d6ae54e7b6c4c2ff16aa65124f5
Gid Baerridge <gid.berridge@gmail.com>
2021-12-12 19:19:03 +0000

    sticky header

--- index.js
+++ index.js
@@ -38,3 +38,26 @@ function closeMenu() {
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
+
+
+//////////////////////////////////////
+// Sticky header
+const nav = document.querySelector('.navbar');
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height * 2;
+console.log(navHeight);
+
+const stickyNav = function(entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if(!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${navHeight }px`,
+});
+headerObserver.observe(header);
